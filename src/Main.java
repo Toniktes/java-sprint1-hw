ec29702c654ddcd999d69cc6fc3e3f1a56e39e09
AntonTeslia <teslyaanton@yandex.ru>
2022-08-27 11:36:17 +0300

    Добавлен вариант при вводе не числа

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,31 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int userInput;
         StepTracker stepTracker = new StepTracker();
-
         while (true) {
             printMenu();
-            userInput = scanner.nextInt();
-            if (userInput == 1) {
-                System.out.println("Введите номер месяца(от 1 до 12): ");
-                int numberOfMonth = scanner.nextInt();
-                System.out.println("Введите номер дня(от 1 до 30): ");
-                int numberOfDay = scanner.nextInt();
-                System.out.println("Введите количество шагов за день: ");
-                int steps = scanner.nextInt();
-                stepTracker.setSteps(numberOfMonth, numberOfDay, steps);
-            } else if (userInput == 2) {
-                System.out.println("Введите номер месяца(от 1 до 12): ");
-                int numberOfMonth = scanner.nextInt();
-                stepTracker.printStat(numberOfMonth);
-            } else if (userInput == 3) {
-                stepTracker.setGoalBySteps();
-            } else if (userInput == 0) {
-                System.out.println("Выход из программы");
-                break;
+            if (scanner.hasNextInt()) {
+                userInput = scanner.nextInt();
+                if (userInput == 1) {
+                    System.out.println("Введите номер месяца(от 1 до 12): ");
+                    int numberOfMonth = scanner.nextInt();
+                    System.out.println("Введите номер дня(от 1 до 30): ");
+                    int numberOfDay = scanner.nextInt();
+                    System.out.println("Введите количество шагов за день: ");
+                    int steps = scanner.nextInt();
+                    stepTracker.setSteps(numberOfMonth, numberOfDay, steps);
+                } else if (userInput == 2) {
+                    System.out.println("Введите номер месяца(от 1 до 12): ");
+                    int numberOfMonth = scanner.nextInt();
+                    stepTracker.printStat(numberOfMonth);
+                } else if (userInput == 3) {
+                    stepTracker.setGoalBySteps();
+                } else if (userInput == 0) {
+                    System.out.println("Выход из программы");
+                    break;
+                }
             } else {
-                System.out.println("Извините, такой команды пока нет.");
+                System.out.println("Не число");
+                break;
             }
         }
     }
